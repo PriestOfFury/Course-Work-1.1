c16f9c7abc0e3320c851692175d0f8a413c6ae6e
Алеша <elemine1337@gmail.com>
2020-07-01 01:05:46 +0300

    Initial Commit

---
+++ Skillbox Course work 1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Skillbox Course work 1.0
+//
+//  Created by Алёша on 01.07.2020.
+//  Copyright © 2020 Алёша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
